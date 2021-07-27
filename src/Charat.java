749047c80b0a5242541db4d5a35acf1c31c19859
chanhl22 <chanhlee@student.42seoul.kr>
2021-07-27 09:34:11 +0900

    Test

---
+++ src/Charat.java
@@ -0,0 +1,9 @@
+
+public class Charat {
+    public static void main(String[] args) {
+        String s = "helloworld";
+
+        System.out.println(s.charAt(-1));
+        System.out.println(s.charAt(0));
+    }
+}
--- src/Longtest.java
+++ src/Longtest.java
@@ -15,7 +15,19 @@ public class Longtest {
         //3000000000
         //3000000000
 
+        //long a1 = 3000000000; //Error
+        long b1 = 300000000 * 10;
+        long c1 = 300000000;
+        long d1 = 100;
+        //System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1*d1);
+
+        //-1294967296
+        //30000000000
+
         long e = c * 100;
         System.out.println(e);
+        //30000000000
     }
 }
