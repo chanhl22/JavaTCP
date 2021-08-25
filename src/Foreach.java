4192e84861722cd96d6c11e2aa8ff413a04bb537
chanhl22 <chanhlee@student.42seoul.kr>
2021-08-25 16:26:35 +0900

    for each && Iterable

---
+++ src/Foreach.java
@@ -0,0 +1,9 @@
+public class Foreach {
+    public static void main(String[] args) {
+        //배열도 돌아가네..?
+        int[] a = {1, 2, 3, 4, 5};
+        for (int x:a) {
+            System.out.print(x + " ");
+        }
+    }
+}
