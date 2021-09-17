16adc4cbec11904c885df195ce4b20c360b423d2
chanhl22 <chanhlee@student.42seoul.kr>
2021-09-18 08:44:36 +0900

    startsWith & endsWith

---
+++ src/StringstartsWithendsWith.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StringstartsWithendsWith {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = "자바 코딩";
+        System.out.println(s1.startsWith("자바"));
+        //true
+
+        String s2 = "자바 코딩";
+        System.out.println(s1.startsWith("자바 코딩"));
+        //true
+
+        String s3 = "자바 코딩";
+        System.out.println(s1.startsWith("자바코딩"));
+        //false
+
+        String s4 = "자바 코딩";
+        System.out.println(s1.endsWith("자바 코딩"));
+        //true
+
+        String s5 = "자바 코딩";
+        System.out.println(s1.endsWith("자바코딩"));
+        //false
+    }
+}
